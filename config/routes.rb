f9704d2242a5085a85b5168ee0b6fc34dc5f11e4
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-17 14:19:34 -0500

    added routes using resources for players

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  resources :players
 end
+
+#https://guides.rubyonrails.org/routing.html
+# resources :bananas
+# creates seven different routes in your application, all mapping to the Bananas controller:
+
+# GET	/bananas	bananas#index	display a list of all bananas
+# GET	/bananas/new	bananas#new	return an HTML form for creating a new banana
+# POST	/bananas	bananas#create	create a new banana
+# GET	/bananas/:id	bananas#show	display a specific banana
+# GET	/bananas/:id/edit	bananas#edit	return an HTML form for editing a banana
+# PATCH/PUT	/bananas/:id	bananas#update	update a specific banana
+# DELETE	/bananas/:id	bananas#destroy	delete a specific banana
