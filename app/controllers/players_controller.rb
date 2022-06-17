92ae4dc000b9736f1546dd6d11b622ace0dc2dee
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-17 14:12:47 -0500

    added players controller methods

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,38 @@
 class PlayersController < ApplicationController
-end
+    def index
+      @players = Player
+        .order(:player_name)
+  
+      render "index.json.jb"
+    end
+  
+    def create
+      @player = Player.create!(
+        player_name: params[:player_name],
+        wins: 0,
+      )
+      logger.info("New Player #{@player} created")
+      render "show.json.jb", status: 201
+    end
+  
+    def show
+      @player = Player.find_by!(id: params[:id])
+      render "show.json.jb"
+    end
+  
+    def update
+      @player = Player.find_by!(id: params[:id])
+      @player.player_name = params[:player_name]
+      @player.wins = params[:wins]
+      @player.save!
+  
+      render "show.json.jb"
+    end
+  
+    def destroy
+      @player = Player.find_by!(id: params[:id])
+      @player.destroy!
+  
+      render "show.json.jb"
+    end
+  end
