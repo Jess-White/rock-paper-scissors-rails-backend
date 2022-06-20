59a47906aae64c4102e86705c5dcecdb8706210f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2022-06-20 13:13:04 -0500

    added error handling for 401 and 422

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_unauthorized
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_record_invalid
+  
+  protect_from_forgery with: :null_session
+
+  def handle_unauthorized(exception)
+    render status: 401, json: { errors: ["Unauthorized"] }
+  end
+
+  def handle_record_invalid(exception)
+    render status: 422, json: { errors: exception.record.errors.full_messages }
+  end
 end
